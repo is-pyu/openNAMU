57f632af9e1d97ff10684989b243a9c5a58986e0
is-pyu <tom7hong7@gmail.com>
2022-02-07 23:09:08 +0900

    init43

--- app.py
+++ app.py
@@ -744,5 +744,5 @@ if __name__ == "__main__":
 
 app = Flask(__name__)
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 5000)
+  port = int(os.environ.get("PORT", 5000))
   app.run(host="0.0.0.0", port=port)
